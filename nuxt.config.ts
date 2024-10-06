b6f434b9e9ce509705955ce32a89f8c879e7292f
Yo'av Moshe <github@yoavmoshe.com>
2024-10-06 18:21:52 +0200

    remove teatime.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import config from "./teatime.config.ts";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
@@ -16,7 +14,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: config.title,
+      title: "TeaTime",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       link: [
@@ -24,7 +22,7 @@ export default defineNuxtConfig({
           rel: "icon",
           href:
             "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>" +
-            config.icon +
+            "🫖" +
             "</text></svg>",
         },
       ],
